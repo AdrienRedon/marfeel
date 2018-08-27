99165c979cc688e44b43b53296483f50e709d905
Adrien REDON <redon.adrien@gmail.com>
2018-08-27 12:27:12 +0200

    ADD: decimal to number utils function

--- public/assets/js/utils/decorators.js
+++ public/assets/js/utils/decorators.js
@@ -1,7 +1,20 @@
-function number(value) {
+/**
+ * Format big number for prettier print
+ * @param {number} value
+ * @param {number} decimal
+ * @return {string}
+ */
+function number(value, decimal) {
   const str = value.toString()
+    .replace('.', ',')
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 
-  return str.replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  if (decimal && str.includes(',')) {
+    const currentDecimal = str.split(',')[1].length;
+    return str.padEnd(str.length + decimal - currentDecimal, '0');
+  }
+
+  return str;
 }
 
 export {
--- spec/utils/decorators.spec.js
+++ spec/utils/decorators.spec.js
@@ -3,6 +3,8 @@ import { number } from '../../public/assets/js/utils/decorators.js';
 describe('decorators', () => {
   it('format number with a dot every 3 digits starting by the end', () => {
     expect(number(123456789)).toEqual('123.456.789');
-    expect(number(123456789.12)).toEqual('123.456.789,12');
+    expect(number(3456789.12)).toEqual('3.456.789,12');
+    expect(number(123456789.1, 2)).toEqual('123.456.789,10');
+    expect(number(123456789, 2)).toEqual('123.456.789');
   })
 });
